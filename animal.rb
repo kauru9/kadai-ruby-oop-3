4179cecf72cb43a199c58644525f475cb0d3ebab
natu <aya.natu0823@gmail.com>
2021-12-03 09:53:25 +0000

    add ruby-oop-3

---
+++ animal.rb
@@ -0,0 +1,14 @@
+#クラスの定義
+class Animal
+    #インスタンス変数
+    attr_accessor:name, :age
+    def initialize(name,age)
+        self.name = name
+        self.age = age
+    end
+    
+    def say
+        puts "#{self.name}です。#{self.age}歳です。"
+    end
+end
+
